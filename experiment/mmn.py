d9dde203f743804cbca92a1fac152cf703011bc4
john-veillette <johnv@uchicago.edu>
2021-09-23 11:42:06 -0500

    fixes bug in MMN script

--- experiment/mmn.py
+++ experiment/mmn.py
@@ -5,7 +5,7 @@ import numpy as np
 
 TEST_MODE = False # set to true for timing tests, so same stimulus always plays
 
-# I'll often set the seed to the subject number so trial order for  
+# I'll often set the seed to the subject number so trial order for
 np.random.seed(0) # each subject can be recreated exactly
 
 box = RTBox() # device handle with which to send TTL triggers
@@ -13,20 +13,20 @@ box = RTBox() # device handle with which to send TTL triggers
 # randomly assign trials to oddball condition
 oddball = True
 trials = np.random.choice(
-	np.array([oddball, not oddball]), 
-	size = 200, 
+	np.array([oddball, not oddball]),
+	size = 300,
 	p = [1., 0.] if TEST_MODE else [.25, .75]
 	)
 
 
 # start the experiment
 WaitSecs(5.)
-for i, trial in enumerate(trials):
+for i, is_oddball in enumerate(trials):
 
-	print("Starting trial %d . . . %s"%(i, trial is oddball))
+	print("Starting trial %d . . . %s"%(i, is_oddball))
 
 	# select stimulus
-	if trial is oddball:
+	if is_oddball:
 		snd = Sound('A')
 		trigger = 0b10
 	else:
@@ -37,24 +37,16 @@ for i, trial in enumerate(trials):
 	## presentation in both conditions, so that timing cannot vary
 	## systematically between conditions!
 
-	# schedule sound 
+	# schedule sound
 	now = GetSecs()
-	snd.play(when = now + 0.5) 
+	snd.play(when = now + 0.5)
 
 	# try to send TTL trigger at same time as sound
 	WaitSecs(.499)
-	box.TTL(trigger) 
+	box.TTL(trigger)
 
 	# wait for next trial
 	WaitSecs(.5 + np.random.uniform(0, 1))
 
 box.close()
 print("Done.")
-
-
-
-
-
-
-
-
